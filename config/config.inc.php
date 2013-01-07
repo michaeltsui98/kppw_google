3018c195b7791931402a04b0351bddd89b0573f4
michaeltsui98 <michaeltsui98@qq.com>
2013-01-07 15:42:29 +0800

    23

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,8 +1,8 @@
 <?php
 define ( 'ADMIN_UID', '1' );
-define ( 'DBHOST', 'localhost');
+define ( 'DBHOST', '192.168.1.99');
 define ( 'DBNAME', 'kppw_google');
-define ( 'DBUSER', 'root');
+define ( 'DBUSER', 'kppw');
 define ( 'DBPASS', '123456');
 define ( 'DBCHARSET', 'gbk' );
 define ( 'CHARSET', 'gbk' );

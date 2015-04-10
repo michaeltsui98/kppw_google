9b1602ec3bc812f7852a592561b3322fa7ec8a74
michael <michaeltsui98@qq.com>
2015-04-10 08:50:29 +0800

    wewewe

--- db_op.php
+++ db_op.php
@@ -7,7 +7,7 @@ include 'app_boot.php';
 $res = DB::select()->from('witkey_industry')->limit(0, 1)->get_one()->execute();
 //get one field values 
 echo DB::select('indus_id')->from('witkey_industry')->limit(0, 1)->get_count()->execute();
-var_dump($res);
+//var_dump($res);
 
 $re2 = DB::select('indus_id')->from('witkey_industry')->join('left_join witkey_task')
 ->on('witkey_industry.inuds_id', '=', 'witkey_task.indus_id')->limit(0, 1)->execute();
